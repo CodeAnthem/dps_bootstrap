4db0cd94a3c0424095b3d48f205e62d0459b2738
averon <averon666@gmail.com>
2025-10-17 18:02:14 +0200

    Changed output format

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 # ==================================================================================================
 # DPS Project - Bootstrap NixOS - A NixOS Deployment System
 # ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-# Date:          Created: 2025-10-12 | Modified: 2025-10-16
+# Date:          Created: 2025-10-12 | Modified: 2025-10-17
 # Description:   One-liner to start DPS Bootstrap script by downloading repo to /tmp
 # Feature:       Clone or reset repository, check for untracked files, execute target script
 # ==================================================================================================
@@ -23,9 +23,9 @@ readonly REPO_TARGET_SCRIPT="main.sh"
 # =============================================================================
 cloneRepo() {
     if git clone --quiet "$REPO_URL" "$REPO_PATH" 2>/dev/null; then
-        printf " %(%Y-%m-%d %H:%M:%S)T %s %s\n" -1 "✅" "start.sh | Successfully cloned repository" >&2
+        printf " %(%Y-%m-%d %H:%M:%S)T %s\n" -1 "Quickstart | ✅ Successfully cloned repository" >&2
     else
-        printf " %(%Y-%m-%d %H:%M:%S)T %s %s\n" -1 "❌" "start.sh | Failed to clone repository" >&2
+        printf " %(%Y-%m-%d %H:%M:%S)T %s\n" -1 "Quickstart | ❌ Failed to clone repository" >&2
         echo " -> Please check your internet connection and try again"
         exit 1
     fi
@@ -36,9 +36,9 @@ resetRepo() {
     if git -C "$REPO_PATH" fetch origin --quiet \
     && git -C "$REPO_PATH" reset --hard origin/"$(git -C "$REPO_PATH" rev-parse --abbrev-ref HEAD)" --quiet
     then
-        printf " %(%Y-%m-%d %H:%M:%S)T %s %s\n" -1 "✅" "start.sh | Successfully reset repository" >&2;
+        printf " %(%Y-%m-%d %H:%M:%S)T %s\n" -1 "Quickstart | ✅ Successfully reset repository" >&2;
     else
-        printf " %(%Y-%m-%d %H:%M:%S)T %s %s\n" -1 "❌" "start.sh | Failed to reset repository" >&2;
+        printf " %(%Y-%m-%d %H:%M:%S)T %s\n" -1 "Quickstart | ❌ Failed to reset repository" >&2;
         exit 1
     fi
 }
@@ -50,7 +50,7 @@ checkUntrackedFiles() {
     local untracked
     untracked=$(git -C "$REPO_PATH" ls-files --others --exclude-standard)
     if [[ -n "$untracked" ]]; then
-        printf " %(%Y-%m-%d %H:%M:%S)T %s %s\n" -1 "⚠️ " "start.sh | Untracked files detected (potential security risk)" >&2
+        printf " %(%Y-%m-%d %H:%M:%S)T %s\n" -1 "Quickstart | ⚠️ Untracked files detected (potential security risk)" >&2
         # List all untracked files:
         for f in $(git -C "$REPO_PATH" ls-files --others --exclude-standard); do
             echo " - (untracked) ${REPO_PATH}/${f}" >&2
@@ -61,10 +61,10 @@ checkUntrackedFiles() {
         case "${answer^^}" in
             Y)
                 git -C "$REPO_PATH" clean -fdx --quiet
-                printf " %(%Y-%m-%d %H:%M:%S)T %s %s\n" -1 "✅" "start.sh | Untracked files removed" >&2
+                printf " %(%Y-%m-%d %H:%M:%S)T %s\n" -1 "Quickstart | ✅ Untracked files removed" >&2
                 ;;
             *)
-                printf " %(%Y-%m-%d %H:%M:%S)T %s %s\n" -1 "⚠️ " "start.sh | Proceeding with untracked files present" >&2
+                printf " %(%Y-%m-%d %H:%M:%S)T %s\n" -1 "Quickstart | ⚠️ Proceeding with untracked files present" >&2
                 ;;
         esac
     fi
@@ -80,5 +80,5 @@ if [[ -d "$REPO_PATH" ]]; then resetRepo; else cloneRepo; fi
 checkUntrackedFiles
 
 # Execute target script
-printf " %(%Y-%m-%d %H:%M:%S)T %s %s\n" -1 "✅" "start.sh | starting ${REPO_TARGET_SCRIPT} script" >&2
+printf " %(%Y-%m-%d %H:%M:%S)T %s\n" -1 "Quickstart | Starting ${REPO_TARGET_SCRIPT} script" >&2
 exec bash "${REPO_PATH_BOOTSTRAPPER}/${REPO_TARGET_SCRIPT}" "$@"
