0ad7e6c55dab50d1133298ac56cc848cfab71d79
averon <averon666@gmail.com>
2025-10-20 23:04:15 +0200

    Adding configurator

--- bootstrap/lib/configurator_modules/configuration_network.sh
+++ bootstrap/lib/configurator_modules/configuration_network.sh
@@ -63,9 +63,6 @@ network_config_init() {
         NETWORK_CONFIG[$value_key]="$default_value"
         NETWORK_CONFIG[$options_key]="$options"
         
-        # Temporary debug - remove after testing
-        echo "[DEBUG] Stored: $value_key = ${NETWORK_CONFIG[$value_key]}" >&2
-        
         # Check if environment variable exists and override (with DPS_ prefix)
         local env_var_name="DPS_${key}"
         if [[ -n "${!env_var_name:-}" ]]; then
@@ -88,10 +85,6 @@ network_config_get() {
     local action_name="$1"
     local key="$2"
     local get_key="${action_name}__${key}__value"
-    
-    # Temporary debug - remove after testing
-    echo "[DEBUG] Get: action=$action_name key=$key lookup_key=$get_key value=${NETWORK_CONFIG[$get_key]:-EMPTY}" >&2
-    
     echo "${NETWORK_CONFIG[$get_key]:-}"
 }
 
