29c6c8fd7f3b15d18206205166cd30b50ac768ee
averon <averon666@gmail.com>
2025-10-24 02:52:20 +0200

    Small Fixes

--- bootstrap/lib/setupConfiguration.sh
+++ bootstrap/lib/setupConfiguration.sh
@@ -562,7 +562,9 @@ config_menu() {
         # Show current configuration with numbers
         local i=0
         for module in "${modules[@]}"; do
+        echo "i: $i"
             ((++i))
+        echo "i: $i"
             module_display "$module" "$i"
             console ""
         done
