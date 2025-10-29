e4494225fd0b4249b51035ce3194cc9924659a5a
averon <averon666@gmail.com>
2025-10-29 13:07:13 +0100

    Reworked whole script

--- bootstrap/main.sh
+++ bootstrap/main.sh
@@ -13,7 +13,7 @@ set -euo pipefail
 # SCRIPT VARIABLES
 # =============================================================================
 # Meta Data
-readonly SCRIPT_VERSION="3.0.8"
+readonly SCRIPT_VERSION="4.0.0"
 readonly SCRIPT_NAME="Nix Deploy System (a NixOS Bootstrapper)"
 
 # Script Path - declare and assign separately to avoid masking return values
