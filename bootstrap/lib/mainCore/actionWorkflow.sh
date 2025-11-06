1e0e96905b6073c0b506028562b4b0051e694757
averon <averon666@gmail.com>
2025-11-06 22:59:29 +0100

    Debugging

--- bootstrap/lib/mainCore/actionWorkflow.sh
+++ bootstrap/lib/mainCore/actionWorkflow.sh
@@ -55,7 +55,7 @@ echo "1 '$actionName'"
     # Register action
     ACTION_NAMES+=("$actionName")
 echo "2 $actionPath"
-    # ACTION_DATA["${actionName}__path"]="$actionPath"
+    ACTION_DATA["DeployVM__path"]="$actionPath"
 echo "3 $description"
     ACTION_DATA["${actionName}__description"]="$description"
 echo "4"
