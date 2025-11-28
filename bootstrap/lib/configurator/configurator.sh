51335a3d16ab5f5bf9d90c6cad08bfc111278caf
averon <averon666@gmail.com>
2025-11-28 14:44:57 +0100

    WIP

--- bootstrap/lib/configurator/configurator.sh
+++ bootstrap/lib/configurator/configurator.sh
@@ -15,19 +15,19 @@ nds_cfg_init() {
     info "Initializing configurator v4.1..."
     
     # 1. Load logic (foundation)
-    import_dir "./logic" false || {
+    import_dir "${LIB_DIR}/configurator/logic" false || {
         fatal "Failed to load logic"
         return 1
     }
     
     # 2. Load all settingTypes (auto-register)
-    import_dir "./settingTypes" false || {
+    import_dir "${LIB_DIR}/configurator/settingTypes" false || {
         fatal "Failed to load settingTypes"
         return 1
     }
     
     # 3. Load all presets (auto-register settings)
-    import_dir "./presets" false || {
+    import_dir "${LIB_DIR}/configurator/presets" false || {
         fatal "Failed to load presets"
         return 1
     }
