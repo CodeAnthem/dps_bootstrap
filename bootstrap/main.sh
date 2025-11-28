642bcf9e0a090077596bf96a8a9e8c90d5b905da
averon <averon666@gmail.com>
2025-11-28 14:03:52 +0100

    Debugging

--- bootstrap/main.sh
+++ bootstrap/main.sh
@@ -109,7 +109,8 @@ if nds_arg_has "--help"; then _main_help; fi
 # ----------------------------------------------------------------------------------
 # Discover available actions
 nds_action_discover "${SCRIPT_DIR}/../actions" "${DEV_ACTIONS[@]}" "true" || crash "Failed to discover actions"
-info good so far
+info "good so far"
+declare -f info
 exit
 
 tui::body_append "$(date '+%Y-%m-%d %H:%M:%S') Discovered ${#ACTION_NAMES[@]} actions"
